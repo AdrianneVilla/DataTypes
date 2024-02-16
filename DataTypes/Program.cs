61144e18c0dbcaa42dfabad54ee433ee77f1d3a7
villa.286986 <villa.286986@LAB-B-PC20>
2024-02-16 09:00:05 +0800

    Add project files.

---
+++ DataTypes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter the piece/s of Apple: ");
+            int apple = Convert.ToInt32(Console.ReadLine());   
+
+            Console.Write($"Enter the total price of {apple} apple(s):  ");
+            double totalPrice = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"The total price of {apple} apple(s) is {totalPrice} ");
+            Console.WriteLine($"The value of original price is {totalPrice}");
+
+            int convertedPrice = (int)totalPrice;
+            Console.WriteLine($"The value of converted price is {convertedPrice}");
+            
+        }
+    }
+}
